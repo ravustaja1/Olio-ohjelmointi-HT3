29831ee1bb590a1f63abd12a5d9de31300bbe6fd
Taneli Solehmainen <taneli@solehmainen.fi>
2023-04-27 11:18:10 +0300

    First commit

--- app/src/main/java/fi/solehmainen/harjoitustyo/fragments/FightFragment.java
+++ app/src/main/java/fi/solehmainen/harjoitustyo/fragments/FightFragment.java
@@ -7,54 +7,83 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import fi.solehmainen.harjoitustyo.Lutemon;
 import fi.solehmainen.harjoitustyo.R;
+import fi.solehmainen.harjoitustyo.Storage;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FightFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FightFragment extends Fragment {
+    private ArrayList<Lutemon> lutemonsAtArena = new ArrayList<>();
+    private ArrayList<Lutemon> fighters = new ArrayList<>();
+    private LinearLayout linearLayout;
+    private Button fightButton;
+    private TextView textViewBattle;
+    private Random randomN = new Random();
+    //private CheckBox checkBox;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FightFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FightFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FightFragment newInstance(String param1, String param2) {
-        FightFragment fragment = new FightFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private int numchecked = 0;
+    private ArrayList<CheckBox> boxes = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_fight);
+
+        linearLayout = findViewById(R.id.llFighters);
+        textViewBattle = findViewById(R.id.tvBattle);
+        fightButton = findViewById(R.id.btnFight);
+
+        makeCheckBoxes();
+
+        /*
+        When "Start Fight" button is pressed it will be checked that 2 fighters are selected. If the condition applies, the fight starts.
+         */
+        fightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int i = 0;
+                numchecked = 0;
+
+                for (CheckBox c: boxes) {
+                    if (c.isChecked()) {
+                        numchecked++;
+                        boxesChecked.add(c);
+                    }
+                    i++;
+                }
+                if (numchecked == 2) {
+                    int y = 0;
+                    for (CheckBox c1 : boxes){
+
+                        if (boxes.get(y).isChecked()) {
+                            fighters.add(Storage.getInstance().getLutemon(c1.getId()));
+                        }
+                        y++;
+                    }
+                    fight(fighters);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Valitse tasan kaksi taistelijaa!",Toast.LENGTH_SHORT).show();
+                    numchecked = 0;
+                }
+                // Testi tulostus
+                for (Lutemon lutemon : fighters) {
+                    System.out.println(lutemon.getName() + " lisättiin taistelulistaan.");
+                }
+            }
+        });
     }
 
     @Override
@@ -63,4 +92,118 @@ public class FightFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fight, container, false);
     }
+
+    public void makeCheckBoxes() {
+        lutemonsAtArena = Storage.getInstance().getLutemonArrayList();
+        linearLayout.removeAllViews();
+        boxes.clear();
+
+
+        CheckBox checkBox;
+        int i = 0;
+        for (Lutemon l : lutemonsAtArena) {
+            checkBox = new CheckBox(this);
+            checkBox.setText(l.getName() + "(" + l.getColor() + ")");
+            checkBox.setId(i++);
+            linearLayout.addView(checkBox);
+            boxes.add(checkBox);
+        }
+
+    }
+
+    /*
+    Method takes in a list of two fighters who fight each other taking turns until one's HP is < 0. Winner gets a win point, loser a lost match. Lutemons are sent to Home
+    where they recover to full health unless one has a 3rd lost figth which means death.
+     */
+    public void fight(ArrayList<Lutemon> fighters) {
+
+        // Randomize the first attacker
+        int starter = randomStarter();
+        int second = -1;
+        if (starter == 0) {
+            second = 1;
+        } else {
+            second = 0;
+        }
+
+        Lutemon l1 = fighters.get(starter);
+        Lutemon l2 = fighters.get(second);
+
+
+        StringBuilder sb = new StringBuilder();
+        textViewBattle.setText("");
+        sb.append("1: " + l1.getColor() + "(" + l1.getName() + ")" + " att: " + l1.getAttack() + " def: " + l1.getDefense() + " exp: " + l1.getExperience() + " HP: " + l1.getHealth() + "/" + l1.getMaxHealth() + "\n");
+        sb.append("2: " + l2.getColor() + "(" + l2.getName() + ")" + " att: " + l2.getAttack() + " def: " + l2.getDefense() + " exp: " + l2.getExperience() + " HP: " + l2.getHealth() + "/" + l2.getMaxHealth() + "\n");
+
+        while ((l1.getHealth() > 0) || (l2.getHealth() > 0)) {
+
+            sb.append(l1.getColor() + "(" + l1.getName() + ")" + " hyökkää " + l2.getColor() + "(" + l2.getName() + ")" + "\n");
+            l2.defense(l1);
+            if (l2.getHealth() > 0) {
+                sb.append(l2.getColor() + "(" + l1.getName() + ")" + " selvisi hengissä!\n");
+                sb.append("1: " + l1.getColor() + "(" + l1.getName() + ")" + " att: " + l1.getAttack() + " def: " + l1.getDefense() + " exp: " + l1.getExperience() + " HP: " + l1.getHealth() + "/" + l1.getMaxHealth() + "\n");
+                sb.append("2: " + l2.getColor() + "(" + l2.getName() + ")" + " att: " + l2.getAttack() + " def: " + l2.getDefense() + " exp: " + l2.getExperience() + " HP: " + l2.getHealth() + "/" + l2.getMaxHealth() + "\n");
+            } else {
+                l2.setDefeats(1);
+                if (l2.getDefeats() == 3) {
+                    sb.append(l2.getColor() + "(" + l2.getName() + ")" + " kuoli.\n");
+                    l1.setWins(1);
+                    l1.setExperience(1);
+                    Storage.getInstance().removeLutemon(l2.getId());
+                } else {
+                    sb.append(l2.getColor() + "(" + l2.getName() + ")" + " hävisi.\n");
+                    //l2.setDefeats(1);
+                    l1.setWins(1);
+                    l1.setExperience(1);
+                }
+
+                break;
+            }
+
+            sb.append(l2.getColor() + "(" + l2.getName() + ")" + " hyökkää " + l1.getColor() + "(" + l1.getName() + ")" + "\n");
+            l1.defense(l2);
+            if (l1.getHealth() > 0) {
+                sb.append(l1.getColor() + "(" + l1.getName() + ")" + " selvisi hengissä!\n");
+                sb.append("1: " + l1.getColor() + "(" + l1.getName() + ")" + " att: " + l1.getAttack() + " def: " + l1.getDefense() + " exp: " + l1.getExperience() + " HP: " + l1.getHealth() + "/" + l1.getMaxHealth() + "\n");
+                sb.append("2: " + l2.getColor() + "(" + l2.getName() + ")" + " att: " + l2.getAttack() + " def: " + l2.getDefense() + " exp: " + l2.getExperience() + " HP: " + l2.getHealth() + "/" + l2.getMaxHealth() + "\n");
+            } else {
+                l1.setDefeats(1);
+                if (l1.getDefeats() == 3) {
+                    sb.append(l1.getColor() + "(" + l1.getName() + ")" + " kuoli.\n");
+                    l2.setWins(1);
+                    l2.setExperience(1);
+                    Storage.getInstance().removeLutemon(l1.getId());
+                } else {
+                    sb.append(l1.getColor() + "(" + l1.getName() + ")" + " hävisi.\n");
+                    //l1.setDefeats(1);
+                    l2.setWins(1);
+                    l2.setExperience(1);
+                }
+
+                break;
+            }
+
+        }
+        l1.setMaxHealth();
+        l2.setMaxHealth();
+        // Print the fight to the textfield
+        textViewBattle.setText(sb);
+
+        // Prepare for next press of the "Fight" button
+        numchecked = 0;
+        fighters.clear();
+        makeCheckBoxes();
+
+    }
+
+    /*
+    Method used for generating a random number which is later used in fight() to determine the first attacker
+     */
+    public int randomStarter() {
+        int starter = 0;
+        int min=0, max=2;
+        starter = min + randomN.nextInt(max);
+        return starter;
+    }
+
 }
